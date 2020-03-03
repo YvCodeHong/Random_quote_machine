9a3e0020f0555bd1c9faf28532b6f7b105eb5d02
Yvonne Tang <ytang604@gmail.com>
2020-03-03 16:22:42 +0000

    add button component

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = ({ buttonDisplayName, clickHandler }) => (
+    <button onClick={clickHandler}>{buttonDisplayName}</button>
+);
+
+export default Button;
+
+
+//creating a presentational component 
