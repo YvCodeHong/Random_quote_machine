13354a156bbe7dbe4ea82047b0d38e2f3ecb0e72
Yvonne Tang <ytang604@gmail.com>
2020-03-05 18:50:00 +0000

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { random } from 'lodash';
-import './App.css';
+import 'typeface-roboto';
+import { Grid, withStyles } from '@material-ui/core';
 import QuoteMachine from './components/QuoteMachine';
+
+const styles ={
+  container: {
+    alignItems: 'center', 
+    display: 'flex', 
+    height: '100vh',
+  }
+};
 
 class App extends Component {
   constructor(props) {
@@ -39,7 +48,7 @@ class App extends Component {
 
   generateNewQuoteIndex() {
     if (!this.state.quotes.length) {
-      return;
+      return undefined;
     }
     return random(0, this.state.quotes.length - 1);
   }
@@ -50,11 +59,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" id="quote-box">
-        <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex}/>
-      </div>
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+        <Grid xs={11} lg={8} item>
+          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+        </Grid>  
+      </Grid>  
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
