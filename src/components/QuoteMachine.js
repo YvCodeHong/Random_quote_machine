8a0a7f321e284375e9961a62e8cdbbbb3f000568
Yvonne Tang <ytang604@gmail.com>
2020-03-05 20:12:24 +0000

    add twitter icon

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const QuoteMachine = (props) => (
     <Card>
@@ -19,6 +22,9 @@ const QuoteMachine = (props) => (
         </CardContent>
          <CardActions>
           <Button size="small" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+          <IconButton>
+              <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+          </IconButton>
           </CardActions>   
    </Card>
 );
