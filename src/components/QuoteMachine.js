49046e83d9bb745282f36c2f6c3b49541a5910c2
Yvonne Tang <ytang604@gmail.com>
2020-03-05 23:15:32 +0000

    add link to twitter

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button'
-import CardActions from '@material-ui/core/CardActions';
+import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const QuoteMachine = (props) => (
+const QuoteMachine = ({ assignNewQuoteIndex, selectedQuote }) => (
     <Card>
         <CardContent>
-        { 
-            props.selectedQuote ?
-             (
-                 <Typography>
-                    {props.selectedQuote.quote} - {props.selectedQuote.author}
+             <Typography>
+                    {selectedQuote.quote} - {selectedQuote.author}
                 </Typography>
-                ) : null
-        }
         </CardContent>
          <CardActions>
-          <Button size="small" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
-          <IconButton>
+          <Button size="small" onClick={assignNewQuoteIndex}>Next Quote</Button>
+          <IconButton
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=thewebdevcoach`}>
               <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
           </IconButton>
           </CardActions>   
